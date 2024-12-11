d3314435b2ff2a752ebbe928e53e0f93fa7a6039
Amy <1857794+itszn@users.noreply.github.com>
2024-12-10 20:00:29 -0700

    Update talk.py

--- server-a/talk.py
+++ server-a/talk.py
@@ -15,27 +15,30 @@ import requests
 }
 '''
 
-def ask_personal_trainer(question):
+def ask_mcp_server(server_url, tool_call, data):
+    # Create the MCP protocol blob to request a given toolcall
     body = {
       "jsonrpc": "2.0",
       "id": 1,
       "method": "tools/call",
       "params": {
-        "name": "ask_personal_trainer",
+        "name": tool_call,
         "arguments": {
           "title": "User Has Question",
-          "body": question,
+          "body": data,
           "labels": ["health"]
         }
       }
     }
-    res = requests.post('http://localhost:5000/mcp/v1', json=body) 
+    # Makes the http request
+    res = requests.post(f'{server_url}/mcp/v1', json=body) 
     print(res.text)
     data = res.json()
 
+    # Extract the data from the response
     return data.get('result',{}).get('content',[])[0].get('text')
 
 
 
 if __name__ == '__main__':
-    ask_personal_trainer("What should I do for my weight?")
+    ask_mcp_server("http://localhost:5000", "ask_personal_trainer", "What should I do for my weight?")

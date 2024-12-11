69b2cfa631b645ec013fc9844ead663c8d2b2321
itszn <itszn@itszn.com>
2024-12-11 01:43:21 +0000

    Add example mcp server and client proxy

---
+++ example-service/service.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import asyncio
+import subprocess
+from typing import Dict, List
+
+from mcp.server import Server, NotificationOptions
+from mcp.server.models import InitializationOptions
+import mcp.server.stdio
+import mcp.types as types
+
+
